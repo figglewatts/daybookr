f1fb43644fc00b66e90c172cdb89bd44527bec9b
Figglewatts <me@figglewatts.co.uk>
2019-05-12 13:40:13 +0100

    Implemented basic command line app structure

--- README.md
+++ README.md
@@ -15,3 +15,35 @@ $ go install github.com/figglewatts/daybookr/cmd/daybookr
 ```bash
 $ daybookr
 ```
+
+## Design
+- `daybookr` executable installable via `go install ...` can be run to generate a site based on a bunch of templates in the folder it's run in (or optionally by giving a folder as a cmd line argument)
+    - site gets output to `./static/` by default, or a folder given by a cmd line arg
+    - need method for converting a page into a URL?
+    - site base name passed in as cmd line arg (for building URLs)
+- `./templates/*.html` - a bunch of templates that the site uses to generate content
+    - other templates can be included in these templates
+- `./entries/*.md` - journal entries
+- `./pages/*.md` - custom pages content
+- `daybook.yml` - config file for a particular site
+    - loaded as arbitrary YAML
+    - could include:
+        - title and subtitle of journal
+        - pages and their templates/content (to show up in the navbar)
+        - author name
+        - links in footer
+
+## Todo
+- Command line integration
+    - args for custom folder
+    - args for config override
+    - args for custom output dir
+    - arg for site base name
+- Load in config file
+- Site data structure to hold config/args
+- URL generator based on site base name
+- Render site pages based on template combination and config
+- Generate front page (with all posts)
+- Generate bespoke pages from config
+- Generate archive page
+- Generate tag pages
--- cmd/daybookr/daybookr.go
+++ cmd/daybookr/daybookr.go
@@ -1,25 +1,79 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"log"
+	"os"
+	"time"
 
-	"github.com/figglewatts/daybookr/internal/daybookr"
+	"gopkg.in/urfave/cli.v1"
 )
 
-func run() error {
-	_, err := daybookr.CreatePost("template.txt", "first-entry.md")
-	if err != nil {
-		return fmt.Errorf("Could not open post: %v", err)
-	}
-	//fmt.Printf(output)
+type daybookrArgs struct {
+	BaseURL string
+}
 
+const (
+	baseURLArgIndex = iota
+)
+
+func getArgs(c *cli.Context) (daybookrArgs, error) {
+	baseURL := c.Args().Get(baseURLArgIndex)
+	if baseURL == "" {
+		return daybookrArgs{}, errors.New("required positional argument 'BASE-URL' must be present")
+	}
+
+	return daybookrArgs{
+		BaseURL: baseURL,
+	}, nil
+}
+
+func runDaybookr(c *cli.Context) error {
+	args, err := getArgs(c)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf(args.BaseURL)
+
+	// _, err = daybookr.CreatePost("template.txt", "first-entry.md")
+	// if err != nil {
+	// 	return fmt.Errorf("Could not open post: %v", err)
+	// }
 	return nil
 }
 
 func main() {
-	err := run()
+	app := cli.NewApp()
+	app.Name = "daybookr"
+	app.Usage = "generate a journal based on some content, config and templates"
+	app.UsageText = "daybookr [global options] <BASE-URL>"
+	app.ArgsUsage = "BASE-URL - the URL that is the base of your site, used when making URLs, i.e. https://yoursite.com"
+	app.Action = runDaybookr
+	app.Author = "Figglewatts"
+	app.Version = "0.1"
+	app.Compiled = time.Now()
+	app.Copyright = "(c) 2019 Figglewatts"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "input, i",
+			Usage: "folder to build the site from",
+			Value: ".",
+		},
+		cli.StringFlag{
+			Name:  "output, o",
+			Usage: "folder to output static site to",
+			Value: "static",
+		},
+		cli.StringFlag{
+			Name:  "config, c",
+			Usage: "config file to use when building site",
+			Value: "daybook.yml",
+		},
+	}
+
+	err := app.Run(os.Args)
 	if err != nil {
-		log.Fatalf("ERROR: %v", err)
+		fmt.Printf("ERROR: %v\n", err)
 	}
 }
